397322cd5bb94fd265ecb54d780487627838403d
three0s <fexli.y.tian@gmail.com>
2019-09-26 01:29:01 -0400

    fix indentation

--- kys-test-client/main.cpp
+++ kys-test-client/main.cpp
@@ -40,23 +40,23 @@ int main(int argc, char* argv[])
         // should be 1 on both sides
         asio::read(socket1, asio::buffer(&result, sizeof(result)));
         std::cout << result << std::endl;
-	assert(result == 1);
+        assert(result == 1);
         asio::read(socket2, asio::buffer(&result, sizeof(result)));
         std::cout << result << std::endl;
-	assert(result == 1);
+        assert(result == 1);
 
         int rand_data = 42;
         asio::write(socket1, asio::buffer(&rand_data, sizeof(rand_data)));
         int rand_echo;
         asio::read(socket2, asio::buffer(&rand_echo, sizeof(rand_echo)));
         std::cout << rand_echo << std::endl;
-	assert(rand_echo == 42);
+        assert(rand_echo == 42);
 
         asio::write(socket2, asio::buffer(&rand_data, sizeof(rand_data)));
         rand_echo = 0; // try to that 42 back
         asio::read(socket1, asio::buffer(&rand_echo, sizeof(rand_echo)));
         std::cout << rand_echo << std::endl;
-	assert(rand_echo == 42);
+        assert(rand_echo == 42);
 }
 
 {
@@ -83,10 +83,10 @@ int main(int argc, char* argv[])
         // should be 1 and 0 on both sides
         asio::read(socket1, asio::buffer(&result, sizeof(result)));
         std::cout << result << std::endl;
-	assert(result == 1);
+        assert(result == 1);
         asio::read(socket2, asio::buffer(&result, sizeof(result)));
         std::cout << result << std::endl;
-	assert(result == 0);
+        assert(result == 0);
         std::cout << "sleeping for 6s\n";
         std::this_thread::sleep_for(std::chrono::seconds(6));
         // get another 0
